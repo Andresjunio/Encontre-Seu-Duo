53ffc03a445e3a60b6e8ceed6fd5c7014f00b406
Andres Junio <andresestudos@gmail.com>
2023-02-12 22:19:23 -0300

    api config 3

--- server/esports/src/main/java/esports/controller/GameController.java
+++ server/esports/src/main/java/esports/controller/GameController.java
@@ -1,10 +1,15 @@
 package esports.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import esports.repository.GameRepository;
 
 @RestController
 public class GameController {
 
+	@Autowired
+	GameRepository gameRepository;
 	
 	
 	
--- server/esports/src/main/java/esports/controller/PlayerController.java
+++ server/esports/src/main/java/esports/controller/PlayerController.java
@@ -1,10 +1,17 @@
 package esports.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import esports.repository.PlayerRepository;
 
 @RestController
 public class PlayerController {
 
+	@Autowired
+	PlayerRepository playerRepository;
+	
 	
 	
 	
